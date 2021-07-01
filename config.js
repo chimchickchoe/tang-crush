6a5f31b56022f1ec2a9544c32644206b5e3697e3
Chôm chôm <86783351+chimchickchoe@users.noreply.github.com>
2021-07-01 19:41:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Lô đỉ l ơi!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnYes: 'Vẫn cứ là iu Béo <33',
+    btnNo: 'Không, đĩ :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Hạnk lại yêu Phương <3',
+    btnReply: 'Gửi cho Béo <3',
+    reply: 'iu thì iu mà khum iu thì iu <33333333',
+    mess: 'T biết mà 🥰. Nàm sao m có thể bỏ đc t 😘😘',
+    messDesc: 'Đêm lay a qua với iem nhó moah moah',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
